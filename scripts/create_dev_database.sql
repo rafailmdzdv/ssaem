5e52fdd575b221f618d7aab0dc68497200cbfac3
rafailmdzdv <rafayt323@gmail.com>
2026-08-04 17:12:55 +0500

    add test-radar

--- scripts/create_dev_database.sql
+++
@@ -1,8 +0,0 @@
-/*
-This file is used to bootstrap development database locally.
-
-Note: ONLY development database;
-*/
-
-CREATE USER "ssaem" SUPERUSER;
-CREATE DATABASE "ssaem" OWNER "ssaem" ENCODING 'utf-8';
